c6c94e284204aae7656d45cbf0fdf5cc8acec501
MiPigurim <michelepigurim26@gmail.com>
2024-04-02 17:49:37 -0300

    feat: created components header and attendee-list

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className=" flex gap-2">
+      <Header />
+      <AttendeeList />
+    </div>
+  );
 }
