73ed9c532cdf68db191ea29c732bed7be36b0625
MiPigurim <michelepigurim26@gmail.com>
2024-04-02 17:56:48 -0300

    style: add nlw unite icon.svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,9 @@
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
+
 export function Header() {
-  return <div>header</div>;
+  return (
+    <div>
+      <img src={nlwUniteIcon} alt="NLW Unite" />
+    </div>
+  );
 }
